a78094188bff0682ebd565d786a7a2e863d5072a
Suhrob <suhrobgithub22@gmail.com>
2024-09-18 17:33:34 +0500

    new style web site

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('sheetdb-form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    fetch(form.action, {
+        method: "POST",
+        body: new FormData(document.getElementById('sheetdb-form')),
+    }) .then(res => res.json())
+    .then(req => console.log(req))
+})
