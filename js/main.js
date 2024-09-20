a49f71e3e03aba44a0e15f226f15d00a62673a00
Suhrob <suhrobgithub22@gmail.com>
2024-09-20 22:50:05 +0500

    form add alert message page

--- js/main.js
+++ js/main.js
@@ -1,11 +1,50 @@
 const form = document.getElementById('sheetdb-form')
+const cont = document.querySelector('section')
+
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
+    e.preventDefault(); // Formani avtomatik jo'natilishini oldini olish
 
-    fetch(form.action, {
-        method: "POST",
-        body: new FormData(document.getElementById('sheetdb-form')),
-    }) .then(res => res.json())
-    .then(req => console.log(req))
-})
+    const fullName = form['lastName'].value.trim();
+    const phone = form['phone'].value.trim();
+
+    const messageDiv = document.createElement('div');
+    messageDiv.className = 'alert'; // Bu yerda kerakli classlarni qo'shing
+
+    if (!fullName || !phone) {
+        messageDiv.classList.add('alert-danger');
+        messageDiv.innerText = 'Please fill in all fields before submitting!';
+        cont.prepend(messageDiv); // Xabarni birinchi qilib qo'shish
+
+        // Xabarni 1 soniyadan keyin o'chirish
+        setTimeout(() => {
+            messageDiv.remove();
+        }, 2000); // 1000 millisekund = 1 sekund
+    } else {
+        fetch(form.action, {
+            method: "POST",
+            body: new FormData(form),
+        }).then(res => res.json())
+            .then((req) => {
+                messageDiv.classList.add('alert-success');
+                messageDiv.innerText = 'Ma\'lumotlaringiz muvaffaqiyatli yuborildi!';
+                cont.prepend(messageDiv);
+                form.reset(); // Formani tozalash
+
+                // Xabarni 1 soniyadan keyin o'chirish
+                setTimeout(() => {
+                    messageDiv.remove();
+                }, 2000); // 1000 millisekund = 1 sekund
+            }).catch((error) => {
+                messageDiv.classList.add('alert-danger');
+                messageDiv.innerText = 'Something went wrong. Please try again!';
+                cont.prepend(messageDiv);
+
+                // Xabarni 1 soniyadan keyin o'chirish
+                setTimeout(() => {
+                    messageDiv.remove();
+                }, 1000); // 1000 millisekund = 1 sekund
+            });
+    }
+});
+
